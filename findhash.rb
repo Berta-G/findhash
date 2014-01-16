c9cd43fe84dcd172b8f7b8fb59e50c766f58274f
Berta-G <berta.gutierrez@gmail.com>
2014-01-16 15:47:29 +0000

    comment added in findhash

--- findhash.rb
+++ findhash.rb
@@ -2,6 +2,7 @@
 def get_hashtags(post)
   puts post.scan(/\A+#[A-Za-z]+\s+/).map {|text| text.delete("# ") }
 end
+#coment to test branching
 
 puts get_hashtags("hello #world")
 puts get_hashtags("#hello world")
